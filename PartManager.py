bdb8beb42d6d3ea9cf8f09f7a7b212d6a529219b
mohamadjalali <hawak.slardar@gmail.com>
2020-01-11 00:32:41 +0330

    Modified PartManager.py

--- PartManager.py
+++ PartManager.py
@@ -1,4 +1,34 @@
 from tkinter import *
+from db import Database
+
+db = Database('store.db')
+
+
+def populate_list():
+    parts_list.delete(0, END)
+    for row in db.fetch():
+        parts_list.insert(END, row)
+
+
+def add_item():
+    db.insert(part_text.get(), customer_text.get(),
+              retailer_text.get(), price_text.get())
+    parts_list.delete(0, END)
+    parts_list.insert(END, (part_text.get(), customer_text.get(), 
+                            retailer_text.get(), price_text.get()))
+    populate_list()
+
+
+
+def remove_item():
+    pass
+
+def update_item():
+    pass
+
+def clear_text():
+    pass
+
 
 # Create window object
 app = Tk()
@@ -25,14 +55,44 @@ retailer_entry = Entry(app, textvariable=retailer_text)
 retailer_entry.grid(row=1, column=1)
 
 # Price
-Price_text  = StringVar()
+price_text  = StringVar()
 Price_label = Label(app, text='Price', font=('bold', 14), pady=20)
 Price_label.grid(row=1, column=2, sticky=W)
-Price_entry = Entry(app, textvariable=Price_text)
+Price_entry = Entry(app, textvariable=price_text)
 Price_entry.grid(row=1, column=3)
+
+# Parts List (Listbox)
+parts_list = Listbox(app, height=8, width=50, border=0)
+parts_list.grid(row=3, columnspan=3, rowspan=6, pady=20, padx=20)
+
+# Create scrollbar
+scrollbar = Scrollbar(app)
+scrollbar.grid(row=3, column=3)
+
+# Set scoll to listbox
+parts_list.configure(yscrollcommand=scrollbar.set)
+scrollbar.configure(command=parts_list.yview)
+
+# Buttons
+add_btn = Button(app, text='Add Part', width=12, command=add_item)
+add_btn.grid(row=2, column=0, pady=20)
+
+remove_btn = Button(app, text='Remove Part', width=12, command=remove_item)
+remove_btn.grid(row=2, column=1)
+
+update_btn = Button(app, text='Update Part', width=12, command=update_item)
+update_btn.grid(row=2, column=2)
+
+clear_btn = Button(app, text='Clear Input', width=12, command=clear_text)
+clear_btn.grid(row=2, column=3)
+
 
 app.title('Part Manager')
 app.geometry('700x350')
+
+# Populate data
+populate_list()
+
 
 # Start program
 app.mainloop()
